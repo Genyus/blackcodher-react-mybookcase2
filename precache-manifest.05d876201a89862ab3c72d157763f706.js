70d6464701940a8f752858a573994a8c62d5d63c
AffirmedVisionary <iam@charlenegrant.co.uk>
2020-10-13 16:36:41 +0100

    Updates

---
+++ precache-manifest.05d876201a89862ab3c72d157763f706.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "4705de539b2e46e4964825f9a4c71cb2",
+    "url": "/blackcodher-react-mybookcase2/index.html"
+  },
+  {
+    "revision": "44a409d9dbcfa79e7cd4",
+    "url": "/blackcodher-react-mybookcase2/static/css/main.1c91e437.chunk.css"
+  },
+  {
+    "revision": "e33b0be681b99525a3b4",
+    "url": "/blackcodher-react-mybookcase2/static/js/2.8b982c36.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/blackcodher-react-mybookcase2/static/js/2.8b982c36.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "44a409d9dbcfa79e7cd4",
+    "url": "/blackcodher-react-mybookcase2/static/js/main.371a44f8.chunk.js"
+  },
+  {
+    "revision": "d24be0c08303f8cd9285",
+    "url": "/blackcodher-react-mybookcase2/static/js/runtime-main.01888d37.js"
+  }
+]);
