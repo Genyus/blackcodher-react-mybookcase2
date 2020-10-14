9fa8835d9d8129a2c00b4ba0e213443032ef47e8
AffirmedVisionary <iam@charlenegrant.co.uk>
2020-10-14 12:19:15 +0100

    updated footer.js links

--- src/components/Footer.js
+++ src/components/Footer.js
@@ -8,11 +8,11 @@ function Footer() {
             <hr />
                 <h3><u>Project Links</u></h3>
                 <div className="projectLinks">
-                <Link to="/blackcodher-bootcamp">Contents</Link>
-                <Link to="/blackcodher-bootcamp/html/portfolio/index.html">Personal Profile</Link>
-                <Link to="/blackcodher-bootcamp/html/bookshop/index.html">Book Shop</Link>
-                <Link to="/blackcodher-bootcamp/javascript/quiz-along/index.html">Simple Quiz</Link>
-                <Link to="/blackcodher-bootcamp/react-and-react-native/mybookcase2/public/index.html">My Bookcase</Link>
+                <Link to="https://affirmedvisionary.github.io/blackcodher-bootcamp/">Contents</Link>
+                <Link to="https://affirmedvisionary.github.io/blackcodher-bootcamp/html/portfolio/index.html">Personal Profile</Link>
+                <Link to="https://affirmedvisionary.github.io/blackcodher-bootcamp/html/bookshop/index.html">Book Shop</Link>
+                <Link to="https://affirmedvisionary.github.io/blackcodher-bootcamp/javascript/quiz-along/index.html">Simple Quiz</Link>
+                <Link to="https://affirmedvisionary.github.io/blackcodher-react-mybookcase2/">My Bookcase</Link>
                 </div>
                 <p className="copyright"><Link to="mailto:iam@charlenegrant.co.uk">Charlene Grant</Link> 2020</p>
                 </div>
