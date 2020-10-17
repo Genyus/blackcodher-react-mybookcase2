21075d6efd7d1cc920cc221207342ffa8521ccf3
AffirmedVisionary <iam@charlenegrant.co.uk>
2020-10-18 00:01:40 +0100

    Added pagination

--- src/components/BookList.js
+++ src/components/BookList.js
@@ -1,13 +1,36 @@
-import React from 'react';
 import Book from './Book';
+import React, { useState } from "react";
+import Pagination from "react-js-pagination";
+import '../components/bookList.css'
 // import data from './models/books.json';
 // import logo from './logo.svg';
 // import './App.css';
 
 const BookList = (props) => {
+  const booksPerPage = 8
+  const [ activePage, setCurrentPage ] = useState( 1 )
+  const indexOfLastBook  = activePage * booksPerPage;
+  const indexOfFirstBook = indexOfLastBook - booksPerPage;
+  const currentBooks = props.books.slice( indexOfFirstBook, indexOfLastBook );
+  const renderBooks = currentBooks.map( ( book) => {
+    return <Book key={book.id} book={book} addBook={props.addBook} removeBook={props.removeBook}/>
+ } );
+  const handlePageChange = ( pageNumber ) => {
+    console.log( `active page is ${ pageNumber }` );
+    setCurrentPage( pageNumber )
+ };
   return (
     <>
-        {props.books.map(book => (<Book key={book.id} book={book} addBook={props.addBook} removeBook={props.removeBook}/>))}
+    {renderBooks}
+        <div className="pagination">
+            <Pagination
+               activePage={ activePage }
+               itemsCountPerPage={ 3 }
+               totalItemsCount={ props.books.length }
+               pageRangeDisplayed={ 3 }
+               onChange={ handlePageChange }
+            />
+         </div>
     </>
   );
 }
