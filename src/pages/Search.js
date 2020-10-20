444b6bb2c38ec1b836a0e68db936fb3c2a964dcc
AffirmedVisionary <iam@charlenegrant.co.uk>
2020-10-20 23:27:34 +0100

    Converted search form to uss bootstrap

--- src/pages/Search.js
+++ src/pages/Search.js
@@ -1,4 +1,8 @@
-import React from 'react';
+import React, { Fragment }from 'react';
+import Form from 'react-bootstrap/Form'
+import Row from 'react-bootstrap/Row'
+import Button from 'react-bootstrap/Form'
+import './search.css'
 
 const Search = (props) => {
 
@@ -13,18 +17,40 @@ function handleSubmit(event) {
 }
 
     return (
-    <form className="search-form" onSubmit={handleSubmit}>
-        <label>Search </label>
-        <input type="text" value={props.keyword} onChange={(e) => props.setKeyword(e.target.value)} />
-        <label>Search Author </label>
-        <input type="text" value={props.authorKeyword} onChange={(e) => props.setAuthorKeyword(e.target.value)} />
-        <label>Search Title </label>
-        <input type="text" value={props.titleKeyword} onChange={(e) => props.setTitleKeyword(e.target.value)} />
-        <label>Search Subject </label>
-        <input type="text" value={props.subjectKeyword} onChange={(e) => props.setSubjectKeyword(e.target.value)} />
-        <button>Submit</button>
-    </form>
+        <Fragment>
+            <div className="search-container">
+                <h4>Choose one of the following search queries:</h4>
+                <Form inline onSubmit={handleSubmit} className="search-form">
+                    <Form.Group as={Row} controlId="searchKeyword">
+                        <Form.Label srOnly>Search Keyword</Form.Label>
+                        <Form.Control className="search-field" type="text" value={props.keyword} onChange={(e) => props.setKeyword(e.target.value)} placeholder="Keyword" />
+                        
+                        <Form.Label srOnly>Search by Author</Form.Label>
+                        <Form.Control className="search-field" type="text" value={props.authorKeyword} onChange={(e) => props.setAuthorKeyword(e.target.value)} placeholder="Author" />
+                        
+                        <Form.Label srOnly>Search by Title</Form.Label>
+                        <Form.Control className="search-field" type="text" value={props.titleKeyword} onChange={(e) => props.setTitleKeyword(e.target.value)} placeholder="Title" />
+                        
+                        <Form.Label srOnly>Search by Subject</Form.Label>
+                        <Form.Control className="search-field" type="text" value={props.subjectKeyword} onChange={(e) => props.setSubjectKeyword(e.target.value)} placeholder="Subject" />
+                    </Form.Group>
+                    <Button as="input" type="submit" value="Submit" />{' '}
+                </Form>
+            </div>
+          </Fragment>
     )
 }
 
 export default Search
+
+    //  <form className="search-form" onSubmit={handleSubmit}>
+    //     <label>Search </label>
+    //     <input type="text" value={props.keyword} onChange={(e) => props.setKeyword(e.target.value)} />
+    //     <label>Search Author </label>
+    //     <input type="text" value={props.authorKeyword} onChange={(e) => props.setAuthorKeyword(e.target.value)} />
+    //     <label>Search Title </label>
+    //     <input type="text" value={props.titleKeyword} onChange={(e) => props.setTitleKeyword(e.target.value)} />
+    //     <label>Search Subject </label>
+    //     <input type="text" value={props.subjectKeyword} onChange={(e) => props.setSubjectKeyword(e.target.value)} />
+    //     <button>Submit</button>
+    // </form>
