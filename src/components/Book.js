f676837f5850abc73b6b4bb9ca0a7d693c641109
AffirmedVisionary <iam@charlenegrant.co.uk>
2020-10-29 23:45:29 +0000

    removed code commented out

--- src/components/Book.js
+++ src/components/Book.js
@@ -6,36 +6,12 @@ import Row from 'react-bootstrap/Row'
 import Col from 'react-bootstrap/Col'
 import Button from 'react-bootstrap/Button'
 
-// import addBook from '../App'
-// import Book from '../models/books.json'
-
 const Book = (props) => {
 
     let {id, volumeInfo: {title, authors, description, imageLinks:{smallThumbnail}},
         saleInfo:{listPrice}} 
         = props.book;
    
-
-        // let amount = () => {
-        //     if (saleInfo.retailPrice) {
-        //         let amount = saleInfo.retailPrice.amount;
-        //         return amount
-        //     }else {
-        //         let amount = 'No Amount Set'; 
-        //         return amount
-        //     }
-        // };
- 
-        // let url = () => {
-        // if (imageLinks) {
-        //     let url = imageLinks.smallThumbnail;
-        //     return url;
-        // } else {
-        //     let url = 'image not available';
-        //     return url;
-        // }
-        // };
-
     return (
         <>
         <Container>
@@ -53,7 +29,7 @@ const Book = (props) => {
             <p className="bookDescription">{description}</p>
             </Col>
             <Col lg="2">
-            <p className="bookPrice">£{listPrice && listPrice.amount}</p>
+            <p className="bookPrice">Retail Price: £{listPrice && listPrice.amount}</p>
             {props.addBook && (
                 <Button variant="warning" onClick={() => props.addBook(title, id)}>Add +</Button>
             )}
