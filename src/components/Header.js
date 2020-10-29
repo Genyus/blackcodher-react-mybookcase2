ab57f58be99b28674c6eb7773bec4b84911a7df5
AffirmedVisionary <iam@charlenegrant.co.uk>
2020-10-29 23:44:59 +0000

    removed dropdown item

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,27 +1,19 @@
 import React from 'react'
-// import { Link } from 'react-router-dom'
 import "../App.css"
 import Navbar from 'react-bootstrap/Navbar'
-import NavDropdown from 'react-bootstrap/NavDropdown'
 import Nav from 'react-bootstrap/Nav'
 
 function Header() {
     return(
         <Navbar sticky="top" collapseOnSelect expand="lg" bg="dark" variant="dark">
-            <Navbar.Brand href="/">The Bookcase</Navbar.Brand>
+            <Navbar.Brand id="navTitle" href="/">The Bookcase</Navbar.Brand>
             <Navbar.Toggle aria-controls="responsive-navbar-nav" />
             <Navbar.Collapse id="responsive-navbar-nav">
              <Nav className="mr-auto">
                 <Nav.Link href="/">Home</Nav.Link>
-                <Nav.Link href="/pages/About">About</Nav.Link>
-                <Nav.Link href="/pages/Search">Search</Nav.Link>
-            <NavDropdown title="Dropdown" id="collasible-nav-dropdown">
-                <NavDropdown.Item href="#action/3.1">Food</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.2">Subject2</NavDropdown.Item>
-                <NavDropdown.Item href="#action/3.3">Subject3</NavDropdown.Item>
-                <NavDropdown.Divider />
-                <NavDropdown.Item href="#action/3.4">Separated link</NavDropdown.Item>
-            </NavDropdown>
+                <Nav.Link href="/about">About</Nav.Link>
+                <Nav.Link href="/search">Search</Nav.Link>
+                <Nav.Link href="/bookcase">Shelf</Nav.Link>
             </Nav>
             </Navbar.Collapse>
             <Nav>
